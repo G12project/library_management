bfc18c9c49ba4bd929fc77811fab725897ee9e22
Yashbontala <cse190001067@iiti.ac.in>
2021-06-06 02:40:05 +0530

    jhdj

--- flask/routes/libauth.py
+++ flask/routes/libauth.py
@@ -5,23 +5,21 @@ from .db import mysql
 libauth=Blueprint('libauth', __name__)
 
 
-@libauth.route('/libregistration/', methods=['GET', 'POST'])
+@libauth.route('/libregistration/', methods=['POST'])
 def libreg():
     if request.method == "POST":
-        form = libRegisterForm(request.form)
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        username=form.username.data
-        address=form.address.data
-        email = form.email.data
+        form =request.get_json() 
+        hashed_password = generate_password_hash(form['password'], method='sha256')
+        username=form['username']
+        address=form['address']
+        email = form['email']
         con=mysql.connection
         cur = con.cursor()
         cur.execute("INSERT INTO librarian(name, email, password, address) VALUES (%s,%s, %s %s)", (username,email,hashed_password,address,))
         con.commit()
         cur.close()
-        flash('Thank You for registering')
-        return redirect(url_for('libhome'))
-    form=libRegisterForm()
-    return render_template('addlibrarian.html',form=form)
+        return make_response(jsonify({'message':'Done'}), 201)
+    return make_response(jsonify({'message':'Error in Registration'}), 404)
 @libauth.route('/liblogin', methods=['GET', 'POST'])
 def liblogin():
     if request.method=="POST":
